62d2992dfdd4a4621a00cb0d5ae2dcd16d7d0716
T-M0516 <mmk.patipati@gmail.com>
2024-07-14 20:13:55 +0900

    ヘッダーとフッター作成

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// ハンバーガーメニュー開閉
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+if(burger) {
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  })
+}
+
