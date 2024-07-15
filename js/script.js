bc4b0a0a7614b2402b121ec427480d74f8a375df
T-M0516 <mmk.patipati@gmail.com>
2024-07-15 23:26:51 +0900

    homeページ-メニューセクション途中

--- js/script.js
+++ js/script.js
@@ -9,3 +9,34 @@ if(burger) {
   })
 }
 
+// 情報バナー表示
+document.addEventListener('DOMContentLoaded', function() {
+  const banner = document.querySelector('.banner-pc');
+  const scrollTrigger = 700; // スクロールして700px経過したら表示
+
+  function toggleBanner() {
+      if (window.scrollY >= scrollTrigger) {
+          banner.classList.add('show');
+      } else {
+          banner.classList.remove('show');
+      }
+  }
+
+  window.addEventListener('scroll', toggleBanner);
+});
+
+// topボタン
+document.addEventListener('DOMContentLoaded', function() {
+  const banner = document.querySelector('.top-btn');
+  const scrollTrigger = 700; // スクロールして700px経過したら表示
+
+  function toggleBanner() {
+      if (window.scrollY >= scrollTrigger) {
+          banner.classList.add('show');
+      } else {
+          banner.classList.remove('show');
+      }
+  }
+
+  window.addEventListener('scroll', toggleBanner);
+});
